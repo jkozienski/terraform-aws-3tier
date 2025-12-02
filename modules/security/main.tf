30b85b75070fb686c95237c3bb04ad04436395f3
jkozienski <kuba.kozienski2203@gmail.com>
2025-12-02 19:23:21 +0100

    Working terraform + ansible setting up webiste without certificates

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -100,6 +100,16 @@ resource "aws_vpc_security_group_ingress_rule" "todolist_app_ssh_from_web" {
   to_port     = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "todolist_app_from_alb" {
+  security_group_id            = aws_security_group.todolist_app.id
+  referenced_security_group_id = aws_security_group.alb.id
+
+  ip_protocol = "tcp"
+  from_port   = 8000
+  to_port     = 8000
+}
+
+
 resource "aws_vpc_security_group_egress_rule" "todolist_app_all" {
   security_group_id = aws_security_group.todolist_app.id
   cidr_ipv4         = "0.0.0.0/0"
